4730c60a579d3d9bcebe93bd330d173d7533cad0
Nishant-28 <92144362+Nishant-28@users.noreply.github.com>
2022-11-17 09:58:46 +0530

    Update q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -1,42 +1,22 @@
-
-#include<iostream>
+//WAP to create a function template to swap 2 numbers.
+#include <iostream>
 using namespace std;
 
-template<class T>
-class student
+template <class T>
+void swapp(T &a, T &b)
 {
-    T name;
-    T roll;
-    T marks;
-public:
-    void getdata()
-    {
-        cout<<"Enter name: ";
-        cin>>name;
-        cout<<"Enter roll: ";
-        cin>>roll;
-        cout<<"Enter marks: ";
-        cin>>marks;
-    }
-    void display()
-    {
-        cout<<"Name: "<<name<<endl;
-        cout<<"Roll: "<<roll<<endl;
-        cout<<"Marks: "<<marks<<endl;
-    }
-};
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
 
 int main()
 {
-    student<int> s1;
-    student<char> s2;
-    cout<<"Enter details of student 1: "<<endl;
-    s1.getdata();
-    cout<<"Enter details of student 2: "<<endl;
-    s2.getdata();
-    cout<<"Details of student 1: "<<endl;
-    s1.display();
-    cout<<"Details of student 2: "<<endl;
-    s2.display();
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    swapp(a, b);
+    cout << "Numbers after swapping: " << a << " " << b << endl;
     return 0;
 }
